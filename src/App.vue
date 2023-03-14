f57733a4df7a6b59bf38986d12877713fc1912a8
Daniel Staszek <staszekdaniel5@gmail.com>
2023-03-14 10:26:29 +0100

    docs: ✏️ add example of usage self service in vue

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { provide} from "vue";
+import Maxio from '@maxio-com/self-service';
+
+let componentsFactory = new window.Maxio.Components({
+  accessTokenUrl:
+      'https://link-to-your-access-token-endpoint.com',
+  onAuthenticationRequest: () => ({
+    customHeader: 'customHeaderValue',
+  }),
+  apiUrl: 'https://link-to-your-api-endpoint.com',
+});
+
+provide('componentsFactory', componentsFactory)
+
+</script>
+
+<template>
+  <div>
+    <h1>Maxio Components</h1>
+  </div>
+  <div class="wrapper">
+  <router-link to="/customer-details">Go to Customer Details</router-link>
+  <router-link to="/subscription-manager">Go to Subscription Manager</router-link>
+  <router-link to="/billing-history">Go to Billing History</router-link>
+  </div>
+  <router-view></router-view>
+</template>
+
+<style>
+  .wrapper{
+    margin: 16px 0;
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+  }
+</style>
---
+++ src/components/BillingHistory.vue
@@ -0,0 +1,14 @@
+<script setup>
+import {ref, onMounted, inject} from "vue";
+
+const root = ref();
+
+onMounted(()=>{
+  inject('componentsFactory').create('billing-history').render(root.value);
+})
+
+</script>
+
+<template>
+  <div ref="root"/>
+</template>
