db982a9532cc199c445639531e1f73ebb6b95641
Mike Pawlinski <michal.pawlinski@maxio.com>
2023-06-01 11:47:35 +0200

    feat: update colors & add npm lockfile

--- src/styles/Button.js
+++ src/styles/Button.js
@@ -1,29 +1,33 @@
 export const ButtonSettings = {
-    componentVariants: {
-        color: {
-            prop: 'colorVariant',
-            variants: {
-                'solid-primary': {
-                    backgroundColor: 'primary.main',
-                    // backgroundColor: 'orangered',
+  componentVariants: {
+    color: {
+      prop: "colorVariant",
+      variants: {
+        "solid-primary": {
+          backgroundColor: "primary.main",
+          // backgroundColor: 'orangered',
 
-                    boxShadow: 'none',
-                    '&:hover': {
-                        // backgroundColor: 'primary.dark',
-                        backgroundColor: '#FF0000',
-                    }
-                },
-                'solid-secondary': {
-                    backgroundColor: 'transparent',
-                    boxShadow: 'none'
-                },
-                'outline-primary': {
-                    boxShadow: 'none'
-                },
-                'outline-secondary': {
-                    boxShadow: 'none'
-                }
-            },
+          boxShadow: "none",
+          "&:hover": {
+            // backgroundColor: 'primary.dark',
+            backgroundColor: "#FF0000",
+          },
         },
+        "solid-secondary": {
+          color: "primary.extraLight",
+          backgroundColor: "transparent",
+          boxShadow: "none",
+          "&:hover": {
+            backgroundColor: "primary.dark",
+          },
+        },
+        "outline-primary": {
+          boxShadow: "none",
+        },
+        "outline-secondary": {
+          boxShadow: "none",
+        },
+      },
     },
-}
+  },
+};
